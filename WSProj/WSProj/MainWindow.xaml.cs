c892e657036c90a8bc23afab657df7015a30d2af
maxarnheiter <maxarnheiter@gmail.com>
2017-02-22 14:01:50 -0500

    2. Initial Project Setup

---
+++ WSProj/WSProj/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO.Ports;
+
+namespace WSProj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string[] _portNames;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeComPorts();
+        }
+
+        void InitializeComPorts()
+        {
+            _portNames = SerialPort.GetPortNames();
+
+            if (_portNames == null)
+            {
+                DebugLog("ERROR: No Com Ports found.");
+                return;
+            }
+
+            DebugLog("Com Ports Found: ");
+
+            foreach (var port in _portNames)
+            {
+                DebugLog("Found: " + port);
+                ComPortComboBox.Items.Add(port);
+            }
+
+            ComPortComboBox.SelectedIndex = 0;
+        }
+
+
+        void DebugLog(string text)
+        {
+            DebuggingTextBox.Text += text + System.Environment.NewLine;
+        }
+    }
+}
